8d432eb34ec8b61951ea9fecee14936220542680
moebiussurfing <moebiussurfing@users.noreply.github.com>
2019-04-25 15:55:04 +0200

    first commit

---
+++ src/ofxBeatClock.h
@@ -0,0 +1,33 @@
+
+#pragma once
+
+#include "ofMain.h"
+
+
+
+#define NUM_SEQ_NOTES 12//max & fixed size
+#define NUM_SEQ_BEATS 16//max & fixed size
+
+class ofxBeatClock
+{
+    
+public:
+    
+    ofxBeatClock()
+    {
+    }
+    
+    ofxBeatClock()
+    {
+    }
+    
+    //-
+    
+    void setup();
+    void update();
+    void draw();
+    void exit();
+
+    
+}
+
