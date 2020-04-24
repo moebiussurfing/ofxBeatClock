f370f9a27fb0e8595b0f23d8f5abb964346fe1d3
moebiussurfing <moebiussurfing@users.noreply.github.com>
2020-04-24 12:20:29 -0300

    starting add link panel to gui

--- 2_ofxBeatClock_example/src/ofApp.cpp
+++ 2_ofxBeatClock_example/src/ofApp.cpp
@@ -1,139 +1,124 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup() {
+void ofApp::setup() 
+{
 	ofBackground(32);
 
 	//-
 
 #ifdef USE_ofxWindowApp
-	//required when opening for first time, when no json settings created yet.
-	windowApp.setSettingsFps(60);
+	windowApp.setSettingsFps(60);//required when opening for first time, when no json settings created yet.
 #else
 	ofSetFrameRate(60);
 	ofSetVerticalSync(true);
 #endif
 
-	//-
+
+	//----
 
 	beatClock.setup();
 
-	////customize positions and sizes
+	//--
+
+	//optional customize all layout elements "grouped" with default settings
+	//beatClock.setPosition_GuiGlobal(10, 10);
+
+	//beatClock.setPosition_GuiExtra(500, 10);
+	//beatClock.setPosition_GuiPanel(100, 100, 300);
+
+	////or customize positions and sizes element by element
 	//beatClock.setPosition_GuiPanel(10, 10, 100);
 	//beatClock.setPosition_BeatBoxes(400, 100, 300);
-	//beatClock.setPosition_BeatBall_Auto(false);//must disable auto to customize pos/size
 	//beatClock.setPosition_BeatBall(400, 400, 50);
 
 	//-
 
-	//callback beat tick
-	listener = beatClock.BeatTick_TRIG.newListener([&](bool&) {this->callback_BeatTick(); });
+	//callback to receive BeatTicks
+	beatListener = beatClock.BeatTick_TRIG.newListener([&](bool&) {this->callback_BeatTick(); });
 }
 
 //--------------------------------------------------------------
 void ofApp::callback_BeatTick()
 {
 	//TODO:
-	//improve callback to get bool value too to avoid to check state like this..
-	if (beatClock.BeatTick_TRIG)
-		ofLogWarning("ofApp") << "TICK! " << beatClock.Beat_current;
+	//could improve callback to get bool value too to avoid to check state like this..
+	if (beatClock.BeatTick_TRIG && false)
+		ofLogWarning("ofApp") << "BeatTick ! " << beatClock.Beat_current;
 }
 
 //--------------------------------------------------------------
-void ofApp::update() {
+void ofApp::update() 
+{
 	beatClock.update();
 
-	////add a log line every x frames to spread received and logged BEAT TICK callbacks
-	//if (ofGetFrameNum() % 15 == 0 && true)
-	//	ofLogWarning("ofApp") << "";
+	//add a log line every x frames to spread received and logged BeatTicks on callbacks
+	if (ofGetFrameNum() % 15 == 0 && false)
+		ofLogWarning("ofApp") << "";
 }
 
 //--------------------------------------------------------------
-void ofApp::draw() {
+void ofApp::draw() 
+{
 	beatClock.draw();
 
-	draw_Anchor(ofGetMouseX(), ofGetMouseY());
+	//--
+
+	if (bDEBUG) draw_Anchor(ofGetMouseX(), ofGetMouseY());
 }
 
 //--------------------------------------------------------------
-void ofApp::exit() {
+void ofApp::exit() 
+{
 	beatClock.exit();
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key) {
-
+void ofApp::keyPressed(int key)
+{
+	//TODO:
 	beatClock.LINK_keyPressed(key);
 
 	//-
 
-	switch (key) 
+	switch (key)
 	{
 
-	case ' '://toggle play
+	case ' '://toggle play/stop
 		beatClock.togglePlay();
 		break;
 
-	case 't'://trig tap when using internal clock only
+	case 't'://trig tap 4 times when using internal clock only to manually set the bpm on the fly
 		beatClock.tap_Trig();
 		break;
 
-	case 'g':
-		beatClock.toggle_Gui_visible();
+	case 'g'://show/hide gui panel
+		beatClock.toggleVisible_GuiPanel();
 		break;
 
 	case OF_KEY_RETURN://get some beatClock info. look api methods into ofxBeatClock.h/.cpp
 		ofLogWarning("ofApp") << "BPM: " << beatClock.getBPM();
 		ofLogWarning("ofApp") << "BAR TIME: " << beatClock.getTimeBar() << "ms";
 		break;
-	
-	case 'd':
-		//DEBUG_Layout = !DEBUG_Layout;
-		beatClock.toggleDebug_Clock();
+
+	case 'd'://enable debug mode
+		beatClock.toggleDebug_Clock();//clock debug
+
+		//beatClock.toggleDebug_Layout();//layout debug
+		//bDEBUG = !bDEBUG;//to show mouse x,y position from ofApp
 		break;
-	
+
 	default:
 		break;
-	
+
 	}
 }
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y) {
-	
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo) {
-
-}
+void ofApp::keyReleased(int key) {}
+void ofApp::mouseMoved(int x, int y) {}
+void ofApp::mouseDragged(int x, int y, int button) {}
+void ofApp::mousePressed(int x, int y, int button) {}
+void ofApp::mouseReleased(int x, int y, int button) {}
+void ofApp::windowResized(int w, int h) {}
+void ofApp::gotMessage(ofMessage msg) {}
+void ofApp::dragEvent(ofDragInfo dragInfo) {}
