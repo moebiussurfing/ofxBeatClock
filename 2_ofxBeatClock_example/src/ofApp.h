81d95d29f6e568eed19dbeb424bcaaf9347325c1
moebiussurfing <moebiussurfing@users.noreply.github.com>
2020-04-24 12:20:29 -0300

    starting add link panel to gui

--- 2_ofxBeatClock_example/src/ofApp.h
+++ 2_ofxBeatClock_example/src/ofApp.h
@@ -12,49 +12,55 @@
 #include "ofxWindowApp.h"
 #endif
 
-class ofApp: public ofBaseApp{
-	public:
-    
-    void setup();
-    void update();
-    void draw();
-    void exit();
-    
-    void keyPressed(int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y);
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
+class ofApp : public ofBaseApp {
+public:
 
-    ofxBeatClock beatClock;
+	void setup();
+	void update();
+	void draw();
+	void exit();
 
-	//callback to receive clock ticks
-	ofEventListener listener;
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+
+	//----
+
+	ofxBeatClock beatClock;
+
+	//callback to receive beat-clock beat-ticks
+	ofEventListener beatListener;
 	void callback_BeatTick();
+
+	//----
+
 
 #ifdef USE_ofxWindowApp
 	ofxWindowApp windowApp;
 #endif
 
-	//anchor debug mark
-	bool DEBUG_Layout = false;
-	void draw_Anchor(int x, int y)//show mouse position and coordinates to debug layout
+	//-
+
+	//debug mouse
+	bool bDEBUG = false;
+	//to show mouse position and x,y coordinates to debug layout
+	void draw_Anchor(int x, int y)
 	{
-		if (DEBUG_Layout)
-		{
-			ofPushStyle();
-			ofFill();
-			ofSetColor(ofColor::red);
-			ofDrawCircle(x, y, 3);
-			int pad;
-			if (y < 15) pad = 20;
-			else pad = -20;
-			ofDrawBitmapStringHighlight(ofToString(x) + "," + ofToString(y), x, y + pad);
-			ofPopStyle();
-		}
+		ofPushStyle();
+		ofFill();
+		ofSetColor(ofColor::red);
+		ofDrawCircle(x, y, 3);
+		int pad;
+		if (y < 15) pad = 20;
+		else pad = -20;
+		ofDrawBitmapStringHighlight(ofToString(x) + "," + ofToString(y), x, y + pad);
+		ofPopStyle();
 	}
 };
