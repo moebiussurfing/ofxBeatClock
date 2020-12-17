5d5a22b2a7d6cdf985e5c0cb3c696e05d9865eec
moebiussurfing <moebiussurfing@users.noreply.github.com>
2020-12-17 01:42:13 -0300

    updated readme. clean
    Former-commit-id: 89aa36f0ced89e45700a2e2a389abef137f37a58

---
+++ example_Basic/src/ofApp.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "ofMain.h"
+
+#include "ofxBeatClock.h"
+
+//uncomment if you added this addon to handle window settings
+#define USE_ofxWindowApp
+#ifdef USE_ofxWindowApp
+#include "ofxWindowApp.h"
+#endif
+
+class ofApp : public ofBaseApp 
+{
+public:
+	void setup();
+	void update();
+	void draw();
+	void exit();
+	void keyPressed(int key);
+
+	//-
+
+	ofxBeatClock beatClock;
+
+	//callback to receive beat-ticks
+	ofEventListener listenerBeat;
+	void Changed_BeatTick();
+
+	//-
+
+#ifdef USE_ofxWindowApp
+	ofxWindowApp windowApp;
+#endif
+
+	bool bDEBUG = false;
+};
