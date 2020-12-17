86a075294355c0710864c6f494aad3254a55d3e7
moebiussurfing <moebiussurfing@users.noreply.github.com>
2020-12-17 01:59:31 -0300

    update
    Former-commit-id: 48a44abd59cd5291a9cb9729f3e6667bfba2c8e8

--- example_Basic/src/ofApp.cpp
+++ example_Basic/src/ofApp.cpp
@@ -7,6 +7,8 @@ void ofApp::setup()
 	//ofSetLogLevel("ofxBeatClock", OF_LOG_VERBOSE);
 	//ofSetLogLevel("ofApp", OF_LOG_VERBOSE);
 
+	ofBackground(ofColor::orangeRed);
+
 	//-
 
 	//window
@@ -19,32 +21,9 @@ void ofApp::setup()
 	ofSetVerticalSync(true);
 #endif
 
-	ofBackground(ofColor::orangeRed);
-
-	//----
+	//-
 
 	beatClock.setup();
-
-	//--
-
-	//optional 
-	//(you can uncomment one of the 3 blocks)
-
-	////customize all layout elements "grouped" with default positions/distances/sizes
-	//beatClock.setPosition_GuiGlobal(200, 50);
-
-	////or customize positions with separated gui panel and gui extra elements
-	//beatClock.setPosition_GuiExtra(500, 10);
-	//beatClock.setPosition_GuiPanel(100, 100, 300);
-
-	////or heavier customize positions and sizes element by element
-	//beatClock.setPosition_GuiPanel(30, 400, 300);
-	//beatClock.setPosition_BpmInfo(ofGetWidth()*0.5 - 100, ofGetHeight() - 50);
-	//beatClock.setPosition_BeatBoxes(10, 10, ofGetWidth() - 20);
-	//beatClock.setPosition_BeatBall(ofGetWidth()*0.5 - 100, ofGetHeight()*0.5 - 100, 200);
-	//beatClock.setPosition_ClockInfo(ofGetWidth() - 200, ofGetHeight()*0.5 - 100);
-
-	//-
 
 	//callback to receive BeatTicks
 	listenerBeat = beatClock.BeatTick_TRIG.newListener([&](bool&) {this->Changed_BeatTick(); });
@@ -53,35 +32,15 @@ void ofApp::setup()
 //--------------------------------------------------------------
 void ofApp::Changed_BeatTick()
 {
-	//TODO:
-	//could improve callback to get bool value too to avoid to check state like this..
-	if (beatClock.BeatTick_TRIG && false)
-		ofLogWarning("ofApp") << "BeatTick ! " << beatClock.Beat_current;
+	if (beatClock.BeatTick_TRIG) ofLogWarning("ofApp") << "BeatTick ! " << beatClock.Beat_current;
 }
 
 //--------------------------------------------------------------
 void ofApp::update()
 {
-	beatClock.update();
-
-	//add a log line every x frames to spread received and logged BeatTicks on callbacks
+	//add a log line every x frames to spread 
+	//received and logged BeatTicks on callbacks
 	if (ofGetFrameNum() % 15 == 0 && false) ofLogWarning("ofApp") << "";
-}
-
-//--------------------------------------------------------------
-void ofApp::draw()
-{
-	beatClock.draw();
-
-	//--
-
-	if (bDEBUG) ofxSurfingHelpers::draw_Anchor(ofGetMouseX(), ofGetMouseY());
-}
-
-//--------------------------------------------------------------
-void ofApp::exit()
-{
-	//beatClock.exit();
 }
 
 //--------------------------------------------------------------
@@ -89,7 +48,6 @@ void ofApp::keyPressed(int key)
 {
 	switch (key)
 	{
-
 		//toggle play/stop
 	case ' ':
 		beatClock.setTogglePlay();
@@ -108,21 +66,15 @@ void ofApp::keyPressed(int key)
 		ofLogWarning("ofApp") << "BAR TIME: " << beatClock.getTimeBar() << "ms";
 		break;
 
-		//enable debug
+		//debug
 	case 'd':
 		beatClock.toggleDebug_Clock();//clock debug
-		//beatClock.toggleDebug_Layout();//layout debug
+		beatClock.toggleDebug_Layout();//layout debug
 		break;
 
-		//enable debug
-	case 'D':
-		bDEBUG = !bDEBUG;//to show mouse x,y position from ofApp
-		break;
-
-		//show/hide gui panel
+		//show/hide gui
 	case 'g':
 		beatClock.toggleVisible_GuiPanel();
 		break;
-
 	}
 }
