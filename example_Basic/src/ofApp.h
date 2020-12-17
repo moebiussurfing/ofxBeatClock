848e04494f8326e4a8ad8c5c20e672cbac39a18b
moebiussurfing <moebiussurfing@users.noreply.github.com>
2020-12-17 01:59:31 -0300

    update

--- example_Basic/src/ofApp.h
+++ example_Basic/src/ofApp.h
@@ -14,8 +14,6 @@ class ofApp : public ofBaseApp
 public:
 	void setup();
 	void update();
-	void draw();
-	void exit();
 	void keyPressed(int key);
 
 	//-
@@ -31,6 +29,4 @@ public:
 #ifdef USE_ofxWindowApp
 	ofxWindowApp windowApp;
 #endif
-
-	bool bDEBUG = false;
 };
