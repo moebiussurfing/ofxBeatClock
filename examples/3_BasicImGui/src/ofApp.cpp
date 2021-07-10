97ef8cc021a877785ecaeb152778f59af6b3fcb2
moebius surfing <moebiussurfing@gmail.com>
2021-07-10 03:05:49 -0300

    clean. ImGui fixings
    Former-commit-id: 9479971f4821dc700b4856d78abf944e9807633f

--- examples/3_BasicImGui/src/ofApp.cpp
+++ examples/3_BasicImGui/src/ofApp.cpp
@@ -7,7 +7,8 @@ void ofApp::setup()
 	ofBackground(ofColor::orangeRed);
 
 	beatClock.setup();
-
+	
+	// callback to receive BeatTicks
 	listenerBeat = beatClock.BeatTick_TRIG.newListener([&](bool&) {this->Changed_BeatTick(); });
 }
 
@@ -15,10 +16,4 @@ void ofApp::setup()
 void ofApp::Changed_BeatTick() // callback to receive BeatTicks
 {
 	if (beatClock.BeatTick_TRIG) ofLogNotice("ofApp") << "BeatTick ! #" << beatClock.Beat_current;
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key)
-{
-	beatClock.keyPressed(key);
 }
