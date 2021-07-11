49a60e732f04d3a28e7b44f89168ac2ad738a125
moebius surfing <moebiussurfing@gmail.com>
2021-07-11 05:31:35 -0300

    clean

--- examples/1_Basic/src/ofApp.h
+++ examples/1_Basic/src/ofApp.h
@@ -1,13 +1,15 @@
 #pragma once
+
 #include "ofMain.h"
 
 #include "ofxBeatClock.h"
 
 class ofApp : public ofBaseApp 
 {
+
 public:
+	
 	void setup();
-	void keyPressed(int key);
 
 	ofxBeatClock beatClock;
 
