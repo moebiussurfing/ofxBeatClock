8fcf59d78d4d6076067b3c56eed46166f096710a
moebius surfing <moebiussurfing@gmail.com>
2021-07-18 03:09:56 -0300

    clean
    Former-commit-id: 35efa8b29f5d6b11e6025061532822005e9869c7

--- examples/2_Link/src/ofApp.cpp
+++ examples/2_Link/src/ofApp.cpp
@@ -12,7 +12,7 @@ void ofApp::setup()
 }
 
 //--------------------------------------------------------------
-void ofApp::Changed_BeatTick()// callback to receive BeatTicks
+void ofApp::Changed_BeatTick() // callback to receive BeatTicks
 {
 	if (beatClock.BeatTick_TRIG) ofLogNotice(__FUNCTION__) << "BeatTick ! #" << beatClock.Beat_current;
 }
@@ -20,42 +20,4 @@ void ofApp::Changed_BeatTick()// callback to receive BeatTicks
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key)
 {
-	switch (key)
-	{
-		// toggle play / stop
-	case ' ':
-		beatClock.setTogglePlay();
-		break;
-
-		// trig tap-tempo 4 times 
-		// when using internal clock 
-		// only to set the bpm on the fly
-	case 't':
-		beatClock.tap_Trig();
-		break;
-
-		// get some beatClock info. look api methods into ofxBeatClock.h
-	case OF_KEY_RETURN:
-		ofLogWarning(__FUNCTION__) << "BPM     : " << beatClock.getBPM() << " beats per minute";
-		ofLogWarning(__FUNCTION__) << "BAR TIME: " << beatClock.getTimeBar() << " ms";
-		break;
-
-	//	// debug
-	//case 'd':
-	//	beatClock.toggleDebug_Clock();//clock debug
-	//	beatClock.toggleDebug_Layout();//layout debug
-	//	break;
-
-		// show gui controls
-		#ifdef USE_OFX_GUI_EXTENDED2
-	case 'g':
-		beatClock.toggleVisible_GuiPanel();
-		break;
-#endif
-
-		// show gui previews
-	case 'G':
-		beatClock.setToggleVisible_GuiPreview();
-		break;
-	}
 }
