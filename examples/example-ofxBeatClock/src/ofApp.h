a9beb83d07067810908e384c662d04e1c8351269
moebius surfing <moebiussurfing@gmail.com>
2021-07-24 05:15:59 -0300

    +
    Former-commit-id: ad9136d04dad3abd509d930027028fe3056dbcd0

--- examples/example-ofxBeatClock/src/ofApp.h
+++ examples/example-ofxBeatClock/src/ofApp.h
@@ -3,14 +3,16 @@
 
 #include "ofMain.h"
 
-#include "ofxWindowApp.h"
+#include "ofxWindowApp.h" // -> optional. can be removed.
 
 /*
 	NOTE:
-	This examples show how to sync with Ableton Live using Link.
+	This example shows how to sync with Ableton Live using Link.
 	To run the example out-of-the-box, go into: ofxBeatClock.h",
 	and uncomment the below line (to enable Link): 
 	#define USE_ofxAbletonLink
+	If you want to exclude the Link feature/add-on, just comment the line!
+	//#define USE_ofxAbletonLink
 */
 
 #include "ofxBeatClock.h"
