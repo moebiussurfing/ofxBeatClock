ab85b3ac342648e848aafff7e9e610c7e4db2c2e
moebius surfing <moebiussurfing@gmail.com>
2021-10-27 08:58:20 -0300

    removed native preview clock widget. clean gui

--- examples/example-ofxBeatClock/src/ofApp.h
+++ examples/example-ofxBeatClock/src/ofApp.h
@@ -5,17 +5,6 @@
 
 #include "ofxWindowApp.h" // -> optional. can be removed.
 
-/*
-	NOTE:
-	This example shows how to sync with Ableton Live using Link.
-	To run the example out-of-the-box, go into the top of the file: 
-	ofxBeatClock.h", after OPTIONAL DEFINES
-	and uncomment the below line (to enable Link): 
-	#define USE_ofxAbletonLink
-	If you want to exclude the Link feature/add-on, just comment the line!
-	//#define USE_ofxAbletonLink
-*/
-
 #include "ofxBeatClock.h"
 
 class ofApp : public ofBaseApp 
